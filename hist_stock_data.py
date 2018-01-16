f84aa2880125d8680643a3960500f6854413dc02
Shufang Ci <cishufang@gmail.com>
2018-01-15 22:36:40 -0500

    added calculate_sma()

--- hist_stock_data.py
+++ hist_stock_data.py
@@ -3,17 +3,16 @@ hist_stock_data.py accepts a list of ticker or one txt file contains tickers, on
 retrieves history stock data and save to a dataframe
 """
 
-# import pandas as pd
 import pandas_datareader as pdr
 from datetime import datetime
+import pandas as pd
 
-
-def get(start,end,ticker):
-    _start=datetime.strptime(start,'%Y-%m-%d').date()
+def get_stock(start,end,ticker):
+    _start = datetime.strptime(start,'%Y-%m-%d').date()
     _end = datetime.strptime(end, '%Y-%m-%d').date()
-    stock=pdr.get_data_yahoo(ticker,_start,_end)
+    stock = pdr.get_data_yahoo(ticker,_start,_end)
     return stock
 
 if __name__ == '__main__':
-    aapl = get('2017-01-01','2018-01-01','AAPL')
+    aapl = get_stock('2017-01-01','2018-01-01','AAPL')
     print(aapl.head())
--- ma_2_line.py
+++ ma_2_line.py
@@ -1,8 +1,30 @@
 from datetime import datetime
 from datetime import date
+import numpy as np
 
 import pandas as pd
 now = datetime.today().date()
 # now = datetime.strptime('2018-01-15','%Y-%m-%d').date()
 if(bool(len(pd.bdate_range(now,now)))):
-    with open
+    pass
+
+def calculate_sma(stock, window_slow=30, window_fast=20):
+    stock['fast_rolling'] = stock.Close.rolling(window=window_fast).mean()
+    stock['slow_rolling'] = stock.Close.rolling(window=window_slow).mean()
+    stock['diff'] = stock['slow_rolling'] - stock['fast_rolling']
+    n_row = stock.shape[0]
+    action = ['no'] * n_row
+    for i in range(0, n_row):
+        if stock['diff'][i] > 0 and stock['diff'][i-1] <= 0:
+            action[i] = "buy"
+        if stock['diff'][i] < 0 and stock['diff'][i-1] >= 0:
+            action[i] = "sell"
+    stock['action'] = action
+    return stock
+
+if __name__ == '__main__':
+    from hist_stock_data import get
+
+    stock = get('2017-01-01', '2018-01-01', 'AAPL')
+    stock_sma = calculate_sma(stock, 20, 10)
+    print(stock_sma.tail())
